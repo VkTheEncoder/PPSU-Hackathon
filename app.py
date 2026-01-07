7561a5067fc5fe788a98e6d03646d62457cff275
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2026-01-07 22:40:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def load_yolo_model():
 def get_ai_response(user_question, image, disease_context):
     try:
         # Use Gemini 1.5 Flash (Fast & Multimodal)
-        model = genai.GenerativeModel('gemini-1.5-flash-latest')
+        model = genai.GenerativeModel('gemini-3-flash-preview')
         
         # We give the AI:
         # 1. The Context (What YOLO found)
